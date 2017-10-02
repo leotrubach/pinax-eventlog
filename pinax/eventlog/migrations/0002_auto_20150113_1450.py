d05d769cfedd48345d7323fc000dcc337b97a7ed
Harry <harry@queserser.co.jp>
2017-10-02 15:35:48 +0900

    fixed migration

--- pinax/eventlog/migrations/0002_auto_20150113_1450.py
+++ pinax/eventlog/migrations/0002_auto_20150113_1450.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='log',
             name='content_type',
-            field=models.ForeignKey(to='contenttypes.ContentType', null=True),
+            field=models.ForeignKey(to='contenttypes.ContentType', null=True, on_delete=models.CASCADE),
             preserve_default=True,
         ),
         migrations.AddField(
